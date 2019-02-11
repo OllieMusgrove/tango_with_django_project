c7f4f3bad48745338bec24248d004c44ed8c89dc
Ollie Musgrove <2426628R@student.gla.ac.uk>
2019-02-11 19:03:54 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -27,8 +27,10 @@ def about(request):
     # html = "Rango says here is the about page." + '<a href="/rango/">Index</a>'
     #return HttpResponse(html)
 
-    context_dict = {'boldmessage': "This tutorial has been put together by Ollie Musgrove"}
-    return render(request, 'rango/about.html', context=context_dict)
+    # context_dict = {'boldmessage': "This tutorial has been put together by Ollie Musgrove"}
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 
 def show_category(request, category_name_slug):
