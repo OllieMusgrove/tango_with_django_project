8bd6fd1582050a92fd449a27544e8b95feae754e
Ollie Musgrove <2426628R@student.gla.ac.uk>
2019-02-12 15:40:55 +0000

    Chapter 9

--- rango/views.py
+++ rango/views.py
@@ -67,6 +67,7 @@ def show_category(request, category_name_slug):
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -180,7 +181,8 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    # return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request,'rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
